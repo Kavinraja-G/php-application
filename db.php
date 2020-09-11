d00058da85dbd6beb5cf0fdc2fe1ba7bbffeb97d
Kavinraja-G <kavinraja.ganesan@gmail.com>
2020-09-11 15:16:37 +0530

    db-change

--- db.php
+++ db.php
@@ -1,7 +1,3 @@
 <?php
-	$script_path = 'users.sql';
-	$command = "mysql --user=root -h localhost -D blog_samples < {$script_path}";
-
-	$output = shell_exec($command . '/shellexec.sql');
 	$conn = mysqli_connect("localhost","root","","blog_samples");
 ?>
