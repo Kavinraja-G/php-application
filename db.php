a3bfd6f582d9607390e9510687dc9a1439a5db83
Kavinraja-G <kavinraja.ganesan@gmail.com>
2020-09-11 14:11:47 +0530

    db-change

--- db.php
+++ db.php
@@ -1,14 +1,14 @@
 <?php
-	$fp = file('users.sql', FILE_IGNORE_NEW_LINES | FILE_SKIP_EMPTY_LINES);
-	$query = '';
-	foreach ($fp as $line) {
-		if ($line != '' && strpos($line, '--') === false) {
-			$query .= $line;
-			if (substr($query, -1) == ';') {
-				mysql_query($query);
-				$query = '';
-			}
-		}
-	}
+	// $fp = file('users.sql', FILE_IGNORE_NEW_LINES | FILE_SKIP_EMPTY_LINES);
+	// $query = '';
+	// foreach ($fp as $line) {
+	// 	if ($line != '' && strpos($line, '--') === false) {
+	// 		$query .= $line;
+	// 		if (substr($query, -1) == ';') {
+	// 			mysql_query($query);
+	// 			$query = '';
+	// 		}
+	// 	}
+	// }
 	$conn = mysqli_connect("localhost","root","","blog_samples");
 ?>
